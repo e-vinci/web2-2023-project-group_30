bd0cd93d1679012b8fe10feaeedf0f982de685e3
Maxime <maxime.issa@student.vinci.be>
2023-12-12 13:33:16 +0100

    fix profile page

--- frontend/src/Components/Pages/ProfilPage.js
+++ frontend/src/Components/Pages/ProfilPage.js
@@ -1,26 +1,57 @@
-
-import { getUserSessionData } from '../../utils/auth';
-
-const { username, email, birthdate } = getUserSessionData();
-
-const ProfilePage = () => {
+const ProfilePage = async () => {
   const main = document.querySelector('main');
-  // Creating HTML structure for the profile
-  const profileHTML = `
-    <div class="container">
-     <div class="row justify-content-center">
-         <div class="profile">
-             <h1 class=text-center>Profile</h1>
-             <h2 class=text-center>${username}</h2>
-             <h3 class=text-center>Email: ${email}</h3>
-             <h3 class="text-center">date de naissance: ${birthdate}</3>
-         </div>
-     </div>
-    </div>
+
+  // Récupérer les données de l'utilisateur depuis l'API
+  try {
+    const response = await fetch('api/profil/:username'); // Remplacez par l'URL de votre API
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const userData = await response.json();
+
+    // Données du profil
+    const { username, birthdate, score } = userData;
+    const age = calculateAge(birthdate);
+
+    // Création de la structure HTML pour le profil
+    const profileHTML = `
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="profile">
+            <h1 class="text-center">Profile</h1>
+            <h2 class="text-center">Username: ${username}</h2>
+            <h3 class="text-center">Age: ${age}</h3>
+            <h3 class="text-center">Date de naissance: ${birthdate}</h3>
+            <h3 class="text-center">Score: ${score}</h3>
+          </div>
+        </div>
+      </div>
     `;
 
-  // Setting the HTML content to the main element
-  main.innerHTML = profileHTML;
+    // Définir le contenu HTML de l'élément principal
+    main.innerHTML = profileHTML;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données de l’utilisateur:', error);
+  }
 };
+
+function calculateAge(birthDate) {
+  const parts = birthDate.split('/');
+  const day = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1;
+  const year = parseInt(parts[2], 10);
+
+  const today = new Date();
+  const birth = new Date(year, month, day);
+
+  let age = today.getFullYear() - birth.getFullYear();
+  const monthDiff = today.getMonth() - birth.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+    // eslint-disable-next-line no-plusplus
+    age--;
+  }
+
+  return age;
+}
 
 export default ProfilePage;
