51b41fe76798ce4f405ae7e6c5a247ea7ecc3b7f
edwinkalinda <edwin.musakalinda@student.vinci.be>
2023-12-12 16:07:16 +0100

    Profil fix

--- frontend/src/Components/Pages/LeaderboardPage.js
+++ frontend/src/Components/Pages/LeaderboardPage.js
@@ -1,7 +1,5 @@
 // import ClearPage from '../../utils/render'
 // Function to fetch player data from the API
-import { getUserSessionData } from '../../utils/auth';
-
 const fetchPlayers = async () => {
   const response = await fetch('/api/users/');
   const players = await response.json();
@@ -11,7 +9,7 @@ const fetchPlayers = async () => {
 const LeaderboardPage = async () => {
   try {
     const players = await fetchPlayers();
-    const currentUser = getUserSessionData().username;
+  
 
     players.sort((a, b) => b.score - a.score); // Sort players by points
 
@@ -24,15 +22,15 @@ const LeaderboardPage = async () => {
             <table class="table table-hover">
               <thead>
                 <tr>
-                  <th class="bg-transparent text-white" scope="col" style="font-size:30px">Joueur</th>
-                  <th class="bg-transparent text-white" scope="col" style="font-size:30px">Score</th>
+                  <th class="bg-transparent text-white" scope="col" style="font-size:35px">Joueur</th>
+                  <th class="bg-transparent text-white" scope="col" style="font-size:35px">Score</th>
                 </tr>
               </thead>
               <tbody>
                 ${players.slice(0, 10).map((player) => `
                   <tr>
-                    <td class="text-white bg-transparent" style="${player.username === currentUser ? 'font-weight: bold;' : ''}">${player.username}</td>
-                    <td class="text-white bg-transparent" style="${player.username === currentUser ? 'font-weight: bold;' : ''}">${player.score}</td>
+                    <td class="text-white bg-transparent">${player.username}</td>
+                    <td class="text-white bg-transparent">${player.score}</td>
                   </tr>
                 `).join('')}
               </tbody>
