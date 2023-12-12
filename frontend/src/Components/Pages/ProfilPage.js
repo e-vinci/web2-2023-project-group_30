84d7a122b34d105015da9bdd94f6d904d19d7c69
edwinkalinda <edwin.musakalinda@student.vinci.be>
2023-12-12 16:07:16 +0100

    Profil fix

--- frontend/src/Components/Pages/ProfilPage.js
+++ frontend/src/Components/Pages/ProfilPage.js
@@ -1,18 +1,20 @@
+import { getUserSessionData } from '../../utils/auth';
+
+const fetchPlayers = async () => {
+  const response = await fetch('/api/users/');
+  const players = await response.json();
+  return players;
+};
 const ProfilePage = async () => {
   const main = document.querySelector('main');
 
   // Récupérer les données de l'utilisateur depuis l'API
   try {
-    const response = await fetch('api/profil/:username'); // Remplacez par l'URL de votre API
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const userData = await response.json();
-
-    // Données du profil
+    const players = await fetchPlayers();
+    const currentUser = getUserSessionData().username;
+    const userData = players.find((player) => player.username === currentUser);
     const { username, birthdate, score } = userData;
-    const age = calculateAge(birthdate);
-
+    
     // Création de la structure HTML pour le profil
     const profileHTML = `
       <div class="container">
@@ -20,7 +22,6 @@ const ProfilePage = async () => {
           <div class="profile">
             <h1 class="text-center">Profile</h1>
             <h2 class="text-center">Username: ${username}</h2>
-            <h3 class="text-center">Age: ${age}</h3>
             <h3 class="text-center">Date de naissance: ${birthdate}</h3>
             <h3 class="text-center">Score: ${score}</h3>
           </div>
@@ -35,23 +36,5 @@ const ProfilePage = async () => {
   }
 };
 
-function calculateAge(birthDate) {
-  const parts = birthDate.split('/');
-  const day = parseInt(parts[0], 10);
-  const month = parseInt(parts[1], 10) - 1;
-  const year = parseInt(parts[2], 10);
-
-  const today = new Date();
-  const birth = new Date(year, month, day);
-
-  let age = today.getFullYear() - birth.getFullYear();
-  const monthDiff = today.getMonth() - birth.getMonth();
-  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
-    // eslint-disable-next-line no-plusplus
-    age--;
-  }
-
-  return age;
-}
 
 export default ProfilePage;
