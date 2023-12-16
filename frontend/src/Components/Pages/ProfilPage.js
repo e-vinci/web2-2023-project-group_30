b2d473cd88ba4ce5a1177fb37d415119cb2f8696
maxime <maxime.issa@student.vinci.be>
2023-12-16 21:18:09 +0100

    visual leaderboard + profil and fixes

--- frontend/src/Components/Pages/ProfilPage.js
+++ frontend/src/Components/Pages/ProfilPage.js
@@ -8,33 +8,33 @@ const fetchPlayers = async () => {
 const ProfilePage = async () => {
   const main = document.querySelector('main');
 
-  // Récupérer les données de l'utilisateur depuis l'API
   try {
     const players = await fetchPlayers();
     const currentUser = getUserSessionData().username;
-    const userData = players.find((player) => player.username === currentUser);
-    const { username, birthdate, score } = userData;
-    
-    // Création de la structure HTML pour le profil
+    const userData = players.find(player => player.username === currentUser);
+    const { username, birthdate, bestscore, stars } = userData; // Assurez-vous d'avoir la propriété 'stars' dans vos données utilisateur
+
     const profileHTML = `
       <div class="container">
         <div class="row justify-content-center">
-          <div class="profile">
-            <h1 class="text-center">Profile</h1>
-            <h2 class="text-center">Username: ${username}</h2>
-            <h3 class="text-center">Date de naissance: ${birthdate}</h3>
-            <h3 class="text-center">Score: ${score}</h3>
+          <div class="col-md-8 profile-container">
+            <h1 class="profile-title">Profil</h1>
+            <div class="profile-info">
+              <h2>Nom d'utilisateur: ${username}</h2>
+              <h3>Date de naissance: ${birthdate}</h3>
+              <h3>Score: ${bestscore}</h3>
+              <h3>Étoiles: ${stars}</h3> <!-- Ajouté les étoiles ici -->
+            </div>
           </div>
         </div>
       </div>
     `;
 
-    // Définir le contenu HTML de l'élément principal
     main.innerHTML = profileHTML;
   } catch (error) {
     console.error('Erreur lors de la récupération des données de l’utilisateur:', error);
   }
 };
 
-
 export default ProfilePage;
+
